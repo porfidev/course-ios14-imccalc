ee90ce60f343084a27b4a2f580cddc68a53c3717
porfi.dev <elporfirio@gmail.com>
2021-07-19 16:46:34 -0500

    Initial Commit

--- IMCCalculator/ViewController.swift
+++ IMCCalculator/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var weightInput: UITextField!
+    @IBOutlet weak var imcResult: UILabel!
+    @IBOutlet weak var imcClassification: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateIMC(_ sender: Any) {
+        // Evitar forzar el valor
+        //let weight = Double(weightInput.text!)!
+        //let height = Double(heightInput.text!)!
+        
+        var weight: Double = 0
+        var height: Double = 0
+        
+        if let wText = weightInput.text {
+            if let wDouble = Double(wText) {
+                weight = wDouble
+            }
+        }
+        
+        if let hText = heightInput.text {
+            if let hDouble = Double(hText) {
+                height = hDouble
+            }
+        }
+        
+        let imc = weight/(height*height)
+        var classification:String
+        
+        if(imc < 18.5) {
+            classification = "Bajo de Peso"
+        } else if imc < 24.9 {
+            classification = "Peso normal"
+        } else if imc < 29.9 {
+            classification = "Sobrepeso"
+        } else {
+            classification = "Obesidad"
+        }
+        
+        imcResult.text = String(format: "%.1f", imc)
+        imcClassification.text = "IMC: \(classification)"
+    }
+    
 }
 
